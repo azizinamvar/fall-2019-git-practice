7a9deef6768e4497793434c50c59d2facf83866e
azizinamvar <58666749+azizinamvar@users.noreply.github.com>
2020-05-30 16:45:39 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
         System.out.println("someone");
+        System.out.println("chenge once");
+
     }
 }
